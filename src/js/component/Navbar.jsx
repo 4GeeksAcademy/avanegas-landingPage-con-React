3da263e26ba7b0b52c1b02f452ca2ec37f3e8ab5
Angie Vanegas P <arco527@gmail.com>
2024-05-14 15:09:19 +0000

    Navbar component

---
+++ src/js/component/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Navbar = ()=>{
+    return (
+        <div className="container-fluid">
+            <nav className="navbar bg-body-tertiary bg-dark">
+                <div className="container-fluid">
+                    <span className="navbar-brand mb-0 h1 text-white">Start Bootstrap</span>
+                    <ul className="nav justify-content-end">
+                        <li className="nav-item">
+                            <a className="nav-link active text-white" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link disabled" href="#">About</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link disabled" href="#">Service</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link disabled" aria-disabled="true">Contact</a>
+                        </li>
+                        </ul>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar;
