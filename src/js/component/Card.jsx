9efcd3af0f4fe893a584074ba5535b2eb286545d
Angie Vanegas P <arco527@gmail.com>
2024-05-14 19:51:30 +0000

    Refact Card

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -2,44 +2,54 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 const Card = () =>{
+
+    const infoCard =[
+        {
+            title:"Card title",
+            text:"Some quick example text to build on the card title and make up the bulk of the card's content.",
+            button:"Go somewhere",
+            labelUrl: "https://4geeks.com/"
+        },
+
+        {
+            title:"Card title",
+            text:"Some quick example text to build on the card title and make up the bulk of the card's content.",
+            button:"Go somewhere",
+            labelUrl: "https://4geeks.com/"
+        },
+
+        {
+            title:"Card title",
+            text:"Some quick example text to build on the card title and make up the bulk of the card's content.",
+            button:"Go somewhere",
+            labelUrl: "https://4geeks.com/"
+        },
+
+        {
+            title:"Card title",
+            text:"Some quick example text to build on the card title and make up the bulk of the card's content.",
+            button:"Go somewhere",
+            labelUrl: "https://4geeks.com/"
+        }
+    ]
+
     return(
         <div className="container">
             <div className="row">
-                <div className="card col-3">
+            {infoCard.map((card, index) =>
+                <div key={index} className="card col-3">
                     <img src={rigoImage} className="card-img-top w-50" alt="..."/>
                     <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                        <h5 className="card-title">{card.title}</h5>
+                        <p className="card-text">{card.text}</p>
+                        <a href={card.labelUrl} className="btn btn-primary">{card.button}</a>
                     </div>
                 </div>
-                <div className="card col-3">
-                    <img src={rigoImage} className="card-img-top w-50" alt="..."/>
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-                <div className="card col-3">
-                    <img src={rigoImage} className="card-img-top w-50" alt="..."/>
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-                <div className="card col-3">
-                    <img src={rigoImage} className="card-img-top w-50" alt="..."/>
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
+            )}
             </div>
         </div>   
     )
 }
+
 
 export default Card
--- src/js/component/Jumbotron.jsx
+++ src/js/component/Jumbotron.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 
 const Jumbotron = () =>{
+    
+    const infoJumbo = {
+        title: "A warm Welcome!",
+        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit",
+        button: "Call to action"
+    }
+
     return (
         <div className="container">
             <div className="jumbotron bg-dark-subtled">
-                <h1 className="display-4">A warm Welcome!</h1>
-                <p className="lead">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus repellendus eius perferendis velit quod, ut corporis, 
-                    dolore officiis dolorem quo, sed consequatur magnam quasi quae ratione corrupti tempore molestias repellat.</p>
+                <h1 className="display-4">{infoJumbo.title}</h1>
+                <p className="lead">{infoJumbo.text}</p>
                 <p className="lead">
-                    <a className="btn btn-primary btn-lg" href="#" role="button">Call to action!</a>
+                    <a className="btn btn-primary btn-lg" href="#" role="button">{infoJumbo.button}</a>
                 </p>
             </div>      
         </div>
