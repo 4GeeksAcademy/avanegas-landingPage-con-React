08274cd6b4bf3b11231184569b257fff23bbe470
Angie Vanegas P <arco527@gmail.com>
2024-05-14 16:36:05 +0000

    Card component

---
+++ src/js/component/Card.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import rigoImage from "../../img/rigo-baby.jpg";
+
+const Card = () =>{
+    return(
+        <div className="container">
+            <div className="row">
+                <div className="card col-3">
+                    <img src={rigoImage} className="card-img-top w-50" alt="..."/>
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                <div className="card col-3">
+                    <img src={rigoImage} className="card-img-top w-50" alt="..."/>
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                <div className="card col-3">
+                    <img src={rigoImage} className="card-img-top w-50" alt="..."/>
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                <div className="card col-3">
+                    <img src={rigoImage} className="card-img-top w-50" alt="..."/>
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>   
+    )
+}
+
+export default Card
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Navbar from "./Navbar";
+import Jumbotron from "./Jumbotron";
+import Card from "./Card";
 
 //include images into your bundle
 // import rigoImage from "../../img/rigo-baby.jpg";
@@ -7,8 +9,10 @@ import Navbar from "./Navbar";
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
+		<div className="conta">
 			<Navbar/>
+			<Jumbotron/>
+			<Card />    
 		</div>
 	);
 };
